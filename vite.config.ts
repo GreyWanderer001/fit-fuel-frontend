29f64221d1d6e1b18c914d2628ae03d316fb6b7c
robert <roberts.shalajevs@gmail.com>
2024-06-06 22:51:40 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
   server: {
     port: 3000,
